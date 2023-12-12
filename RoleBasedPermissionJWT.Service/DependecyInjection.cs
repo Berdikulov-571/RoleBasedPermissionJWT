4120005e0dd160f90daf0b346e6975b15b3ae3c1
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:53:35 +0500

    DependecyInjection

---
+++ RoleBasedPermissionJWT.Service/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace RoleBasedPermissionJWT.Service
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
