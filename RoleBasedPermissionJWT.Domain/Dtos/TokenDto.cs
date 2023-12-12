de29648813bbdb81f5d2eb0bf46fa3ad0004aeda
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:49:18 +0500

    TokenDto

---
+++ RoleBasedPermissionJWT.Domain/Dtos/TokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RoleBasedPermissionJWT.Domain.Dtos
+{
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
