e7f6620d23acc5aef11f4e4586ff075d518d4b59
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:50:47 +0500

    RolePermissions

---
+++ RoleBasedPermissionJWT.Domain/Entities/RolePermission.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoleBasedPermissionJWT.Domain.Entities
+{
+    public class RolePermission
+    {
+        [Key]
+        public int RolePermissionId { get; set; }
+
+        [ForeignKey(nameof(Role))]
+        public int RoleId { get; set; }
+
+        [ForeignKey(nameof(Permission))]
+        public int PermissionId { get; set; }
+
+
+        public Role Role { get; set; }
+        public Permission Permission { get; set; }
+    }
+}
