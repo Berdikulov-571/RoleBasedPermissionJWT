88a15d51429eca014a3e3c8bacc8bc2b405ec5ea
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:48:23 +0500

    ApplicationDbContext

---
+++ RoleBasedPermissionJWT.DataAcess/DataContexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleBasedPermissionJWT.Domain.Entities;
+using RoleBasedPermissionJWT.Service.Abstractions.DataContexts;
+
+namespace RoleBasedPermissionJWT.DataAcess.DataContexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRoles> UserRoles { get; set; }
+    }
+}
