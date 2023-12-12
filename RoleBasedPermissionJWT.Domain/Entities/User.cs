7122cac71c4df58ddef0c173d0a5dcb201f41d39
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:51:31 +0500

    User

---
+++ RoleBasedPermissionJWT.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoleBasedPermissionJWT.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireDate { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+    }
+}
