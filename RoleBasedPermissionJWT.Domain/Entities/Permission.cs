1a2904452cf276a08361ea8c95999da9fe2ba493
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:50:25 +0500

    Permissions

--- RoleBasedPermissionJWT.Domain/Entities/Permission.cs
+++ RoleBasedPermissionJWT.Domain/Entities/Permission.cs
@@ -1,13 +1,15 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace RoleBasedPermissionJWT.Domain.Entities
 {
     public class Permission
     {
+        [Key]
         public int PermissionId { get; set; }
         public string PermissionName { get; set; } = default!;
 
         [JsonIgnore]
-        public List<Role> Roles { get; set; } 
+        public ICollection<Role> Roles { get; set; } 
     }
 }
