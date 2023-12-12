3c9aebae3918cf8d1f9c28968db6fc139e6f683d
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:49:38 +0500

    DependecyInjection

---
+++ RoleBasedPermissionJWT.DataAcess/DependecyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoleBasedPermissionJWT.DataAcess.DataContexts;
+
+namespace RoleBasedPermissionJWT.DataAcess
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return service;
+        }
+    }
+}
