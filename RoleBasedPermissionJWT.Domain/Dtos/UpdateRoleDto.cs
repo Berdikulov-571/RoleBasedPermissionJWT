1a47100f670437f08d7bb01c60e9ef6436a8fb22
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:49:11 +0500

    UpdateRoleDto

---
+++ RoleBasedPermissionJWT.Domain/Dtos/UpdateRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RoleBasedPermissionJWT.Domain.Dtos
+{
+    public class UpdateRoleDto
+    {
+        public string Name { get; set; }
+
+        public ICollection<int> Permissions { get; set; }
+    }
+}
