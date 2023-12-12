a238f729f37c454501028935bac0a7fb5068c306
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:51:52 +0500

    UserRoles

---
+++ RoleBasedPermissionJWT.Domain/Entities/UserRoles.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoleBasedPermissionJWT.Domain.Entities
+{
+    public class UserRoles
+    {
+        [Key]
+        public int UserRoleId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(Role))]
+        public int RoleId { get; set; }
+
+        public Role Role { get; set; }
+        public User User { get; set; }
+    }
+}
