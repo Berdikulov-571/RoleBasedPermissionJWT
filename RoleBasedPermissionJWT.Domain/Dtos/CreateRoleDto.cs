55601e1a741ba1ea8d271e05a21cb24b30bd968a
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:48:57 +0500

    CreateRoleDto

---
+++ RoleBasedPermissionJWT.Domain/Dtos/CreateRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RoleBasedPermissionJWT.Domain.Dtos
+{
+    public class CreateRoleDto
+    {
+        public string Name { get; set; }
+        public List<int> Permissions { get; set; }
+    }
+}
