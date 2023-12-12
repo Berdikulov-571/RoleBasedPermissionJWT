281af7fd374710814f3ab21d8bb95df7516d96f3
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:50:56 +0500

    IApplicationDbContext

---
+++ RoleBasedPermissionJWT.Service/Abstractions/DataContexts/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleBasedPermissionJWT.Domain.Entities;
+
+namespace RoleBasedPermissionJWT.Service.Abstractions.DataContexts
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRoles> UserRoles { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
