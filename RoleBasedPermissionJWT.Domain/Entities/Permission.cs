e2f7f1c6abca392d26250c5e416854d60fce0560
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 15:21:01 +0500

    Добавьте файлы проекта.

---
+++ RoleBasedPermissionJWT.Domain/Entities/Permission.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RoleBasedPermissionJWT.Domain.Entities
+{
+    public class Permission
+    {
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; } = default!;
+
+        [JsonIgnore]
+        public List<Role> Roles { get; set; } 
+    }
+}
---
+++ RoleBasedPermissionJWT.Domain/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RoleBasedPermissionJWT.Domain.Entities
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        [JsonIgnore]
+        public 
+    }
+}
