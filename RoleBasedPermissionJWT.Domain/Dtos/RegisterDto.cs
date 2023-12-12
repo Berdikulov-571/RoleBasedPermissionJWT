19eea8584923b21b2a351405159f818ffdb141e0
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 19:50:31 +0500

    RegisterDto

---
+++ RoleBasedPermissionJWT.Domain/Dtos/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RoleBasedPermissionJWT.Domain.Dtos
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<int> Roles { get; set; }
+    }
+}
