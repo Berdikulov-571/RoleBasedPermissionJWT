ba7b2335fee682081fa7b1e7e9dd72abaacdad80
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 17:04:22 +0500

    TokenService

---
+++ RoleBasedPermissionJWT.Service/Abstractions/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using RoleBasedPermissionJWT.Domain.Dtos;
+using RoleBasedPermissionJWT.Domain.Entities;
+
+namespace RoleBasedPermissionJWT.Service.Abstractions.Interfaces
+{
+    public interface ITokenService
+    {
+        public string GenerateJWT(User user);
+        public ValueTask<TokenDto> RefreshToken(RefreshTokenDto refreshTokenDTO);
+    }
+}
