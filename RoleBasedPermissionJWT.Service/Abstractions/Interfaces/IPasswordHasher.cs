6c998677608886a49c2d0020c0d38e49af63595f
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 17:04:30 +0500

    IPasswordHash

--- RoleBasedPermissionJWT.Service/Abstractions/Interfaces/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RoleBasedPermissionJWT.Service.Abstractions.Interfaces
-{
-    public interface IPasswordHasher
-    {
-        string Encrypt(string password, string salt);
-        bool Verify(string hash,string password,string salt);
-    }
-}
