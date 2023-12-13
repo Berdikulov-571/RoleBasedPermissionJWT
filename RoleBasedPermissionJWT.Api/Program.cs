df15aa579756c9e6388f8567fe224fc57107346a
berdikulov-571 <mygithub571@gmail.com>
2023-12-13 17:03:35 +0500

    Program.cs

--- RoleBasedPermissionJWT.Api/Program.cs
+++ RoleBasedPermissionJWT.Api/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RoleBasedPermissionJWT.DataAcess;
 using RoleBasedPermissionJWT.Service;
+using RoleBasedPermissionJWT.Service.Abstractions.Interfaces;
+using RoleBasedPermissionJWT.Service.Services;
+using System.Text;
 
 namespace RoleBasedPermissionJWT.Api
 {
@@ -11,7 +17,48 @@ namespace RoleBasedPermissionJWT.Api
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+                c.AddSecurityRequirement(securityRequirement);
+            });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                    options =>
+                    {
+                        options.TokenValidationParameters = GetTokenValidationParameters(builder.Configuration);
+
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnAuthenticationFailed = (context) =>
+                            {
+                                if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                                {
+                                    context.Response.Headers.Add("IsTokenExpired", "true");
+                                }
+                                return Task.CompletedTask;
+                            }
+                        };
+                    });
 
             builder.Services.AddService();
             builder.Services.AddDataAccess(builder.Configuration);
@@ -24,14 +71,29 @@ namespace RoleBasedPermissionJWT.Api
                 app.UseSwaggerUI();
             }
 
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
             app.Run();
+        }
+        static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = configuration["JWT:Audience"],
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"])),
+                ClockSkew = TimeSpan.Zero,
+            };
         }
     }
 }
